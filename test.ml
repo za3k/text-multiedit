6ec589225e3348c42351ca055d0ffe9689868100
Zachary Vance <za3k@za3k.com>
2025-02-12 18:42:16 -0500

    Revise tests to be more declarative

--- test.ml
+++ test.ml
@@ -1,75 +1,94 @@
 open Text
 open OUnit2
 
+let all_tests = ref []
+let add_test (test: test) : unit = all_tests := test :: !all_tests
+let run_tests () = run_test_tt_main ("text editor" >::: !all_tests)
+
+let make_test ?printer : ('a * 'a) -> test = function
+    | (x, b) -> "?" >:: (fun _ -> assert_equal ?printer x b)
+
+let add_eq_tests ?printer (label: string) (tests: ('a * 'a) list) : unit =
+    label >::: List.map (make_test ?printer) tests |> add_test
+
+let () = add_eq_tests "nth_char" [
+   ((nth_char 'a' "baaaa" 1), 1);
+   ((nth_char 'a' "baaaa" 2), 2);
+   ((nth_char 'a' "babba" 2), 4);
+   ((nth_char 'a' "aaaa" 1), 0);
+   ((nth_char 'a' "aaaa" 2), 1);
+] 
+
 (*let text = "14\nThis is the first line\nThis is the second line\nThis is the third line\n"*)
-let text = "01\n34\n67\n"
-let text2 = "01\n\n\n"
-let text3 = "\n12\n45\n"
-let empty_text = "\n"
-
-let _ = "test suite for nth_char" >::: [
-   "t1" >:: (fun _ -> assert_equal (nth_char 'a' "baaaa" 1) 1);
-   "t2" >:: (fun _ -> assert_equal (nth_char 'a' "baaaa" 2) 2);
-   "t3" >:: (fun _ -> assert_equal (nth_char 'a' "babba" 2) 4);
-   "t4" >:: (fun _ -> assert_equal (nth_char 'a' "aaaa" 1) 0);
-   "t5" >:: (fun _ -> assert_equal (nth_char 'a' "aaaa" 2) 1);
-] |> run_test_tt_main
-
-let _ = "test suite for nth_line_start" >::: [
-   "line 1.0" >:: (fun _ -> assert_equal (nth_line_start text 0) 0);
-   "line 1.1" >:: (fun _ -> assert_equal (nth_line_start text 1) 3);
-   "line 1.2" >:: (fun _ -> assert_equal (nth_line_start text 2) 6);
-   "line 2.0" >:: (fun _ -> assert_equal (nth_line_start text2 0) 0);
-   "line 2.1" >:: (fun _ -> assert_equal (nth_line_start text2 1) 3);
-   "line 2.2" >:: (fun _ -> assert_equal (nth_line_start text2 2) 4);
-   "empty" >:: (fun _ -> assert_equal (nth_line_start empty_text 0) 0);
-   "line 3.0" >:: (fun _ -> assert_equal (nth_line_start text3 0) 0);
-   "line 3.1" >:: (fun _ -> assert_equal (nth_line_start text3 1) 1);
-   "line 3.2" >:: (fun _ -> assert_equal (nth_line_start text3 2) 4);
+let () =
+let text1 = "01\n34\n67\n"
+and text2 = "01\n\n\n"
+and text3 = "\n12\n45\n" in
+add_eq_tests "nth_line_start" [
+   ((nth_line_start text1 0), 0);
+   ((nth_line_start text1 1), 3);
+   ((nth_line_start text1 2), 6);
+   ((nth_line_start text2 0), 0);
+   ((nth_line_start text2 1), 3);
+   ((nth_line_start text2 2), 4);
+   ((nth_line_start text3 0), 0);
+   ((nth_line_start text3 1), 1);
+   ((nth_line_start text3 2), 4);
+   ((nth_line_start "\n" 0), 0);
 
    (* +1 out of bounds edge case *)
-   "line 1.end" >:: (fun _ -> assert_equal (nth_line_start text 3) 9);
-   "line 2.end" >:: (fun _ -> assert_equal (nth_line_start text2 3) 5);
-   "line 3.end" >:: (fun _ -> assert_equal (nth_line_start text3 3) 7);
-   "empty OOB" >:: (fun _ -> assert_equal (nth_line_start empty_text 1) 0);
-] |> run_test_tt_main
+   ((nth_line_start text1 3), 9);
+   ((nth_line_start text2 3), 5);
+   ((nth_line_start text3 3), 7);
+   ((nth_line_start "\n" 1), 0);
+]
 
-let _ = "test suite for line_of" >::: [
-   "t0" >:: (fun _ -> assert_equal (line_of text 0) (0, 0));
-   "t1" >:: (fun _ -> assert_equal (line_of text 1) (0, 1));
-   "t2" >:: (fun _ -> assert_equal (line_of text 2) (0, 2));
-   "t3" >:: (fun _ -> assert_equal (line_of text 3) (1, 0));
-   "t4" >:: (fun _ -> assert_equal (line_of text 4) (1, 1));
-   "t5" >:: (fun _ -> assert_equal (line_of text 5) (1, 2));
-   "t6" >:: (fun _ -> assert_equal (line_of text 6) (2, 0));
-   "t7" >:: (fun _ -> assert_equal (line_of text 7) (2, 1));
-   "t8" >:: (fun _ -> assert_equal (line_of text 8) (2, 2));
-   "text 2" >:: (fun _ -> assert_equal (line_of text2 3) (1, 0));
-   "text 2" >:: (fun _ -> assert_equal (line_of text2 4) (2, 0));
+let () =
+let text1 = "01\n34\n67\n"
+and text2 = "01\n\n\n" in
+add_eq_tests "test suite for line_of" [
+   ((line_of text1 0), (0, 0));
+   ((line_of text1 1), (0, 1));
+   ((line_of text1 2), (0, 2));
+   ((line_of text1 3), (1, 0));
+   ((line_of text1 4), (1, 1));
+   ((line_of text1 5), (1, 2));
+   ((line_of text1 6), (2, 0));
+   ((line_of text1 7), (2, 1));
+   ((line_of text1 8), (2, 2));
+   ((line_of text2 3), (1, 0));
+   ((line_of text2 4), (2, 0));
 
-   "off left"  >:: (fun _ -> assert_equal (line_of text ~-1) (0, 0));
-   "off right" >:: (fun _ -> assert_equal (line_of text 9) (2, 2));
-] |> run_test_tt_main
+   ((line_of text1 ~-1), (0, 0));
+   ((line_of text1 9), (2, 2));
+]
 
-let _ = "test suite for pos_of" >::: [
-   "t0" >:: (fun _ -> assert_equal (pos_of text 0 0) 0);
-   "t1" >:: (fun _ -> assert_equal (pos_of text 0 1) 1);
-   "t2" >:: (fun _ -> assert_equal (pos_of text 0 2) 2);
-   "t3" >:: (fun _ -> assert_equal (pos_of text 1 0) 3);
-   "t4" >:: (fun _ -> assert_equal (pos_of text 1 1) 4);
-   "t5" >:: (fun _ -> assert_equal (pos_of text 1 2) 5);
-   "t6" >:: (fun _ -> assert_equal (pos_of text 2 0) 6);
-   "t7" >:: (fun _ -> assert_equal (pos_of text 2 1) 7);
-   "t8" >:: (fun _ -> assert_equal (pos_of text 2 2) 8);
-   "text2"           >:: (fun _ -> assert_equal (pos_of text2 1 0) 3);
-   "off right empty" >:: (fun _ -> assert_equal (pos_of text2 1 5) 3);
-   "off left empty"  >:: (fun _ -> assert_equal (pos_of text2 1 ~-5) 3);
+let () =
+let text1 = "01\n34\n67\n"
+and text2 = "01\n\n\n" in
+add_eq_tests "test suite for pos_of" [
+   ((pos_of text1 0 0), 0);
+   ((pos_of text1 0 1), 1);
+   ((pos_of text1 0 2), 2);
+   ((pos_of text1 1 0), 3);
+   ((pos_of text1 1 1), 4);
+   ((pos_of text1 1 2), 5);
+   ((pos_of text1 2 0), 6);
+   ((pos_of text1 2 1), 7);
+   ((pos_of text1 2 2), 8);
+   ((pos_of text2 1 0), 3);
 
-   "off top"    >:: (fun _ -> assert_equal (pos_of text ~-3 2) 0);
-   "off left"   >:: (fun _ -> assert_equal (pos_of text 0 ~-5) 0);
-   "off right"  >:: (fun _ -> assert_equal (pos_of text 2 7) 8);
-   "off bottom" >:: (fun _ -> assert_equal (pos_of text 3 6) 8);
-] |> run_test_tt_main
+   (* off right/left *)
+   ((pos_of text2 1 5), 3);
+   ((pos_of text2 1 ~-5), 3);
 
+   (* off top/left/right/bottom *)
+   ((pos_of text1 ~-3 2), 0);
+   ((pos_of text1 0 ~-5), 0);
+   ((pos_of text1 2 7), 8);
+   ((pos_of text1 3 6), 8);
+]
+
+let () = run_tests ()
 
 
